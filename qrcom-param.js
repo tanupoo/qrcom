ed9836a64c3b69e85b545a6e1f43ddf8038e8b88
tanupoo <github-shoichi@tanu.org>
2021-01-08 21:30:25 +0900

    added comment and changed the order.  the latter should not be changed.

--- qrcom-param.js
+++ qrcom-param.js
@@ -1,3 +1,6 @@
+// language
+const qrcom_lang = 'en';
+
 // maximum data size in byte per a QR code.
 // note: corelation to the number of the version and errorCorrectionLevel.
 // see https://www.qrcode.com/en/about/version.html
@@ -16,9 +19,10 @@ const qrcom_color_done = '#56b4e9';
 const qrcom_color_notyet = '#d55e00';
 const qrcom_color_passing = '#d55e00';
 
-// language
-const qrcom_lang = 'en';
+// interval in milisecond to take an image and trying to get QR code.
+const camera_read_interval = 16;
 
+// ********
 // don't need to touch unless you want to add other language.
 const qrcom_msg = {
     init: function(lang) {
@@ -40,15 +44,17 @@ const qrcom_msg = {
     },
 };
 
-// interval in milisecond to take an image and trying to get QR code.
-const camera_read_interval = 16;
+// ********
+// Below should not be changed.
 
 /*
  * 2 Byte Header
- *   1バイト目: 総フラグメント数を示す。'A'ならば1個。'9'ならば62個。
- *   2バイト目: 何番目のフラグメントかを示す。'A'ならば1個目。'9'ならば62個目。
- *   e.g. QRコードが1個だけの場合は、0x65, 0x65 (AA) となる。
- *   e.g. QRコードが3個あり、1つ目のデータは、0x67, 0x65 (CA) となる。
+ *   1B: The number of fragments. e.g. 'A' means 1. '9' means 62.
+ *   2B: Fragment index. e.g. 'A' means the first fragment.
+ *   e.g. If there is only one fragment, the header of the first fragment
+ *        is 'A', 'A'.
+ *   e.g. If the total number of fragment is 3, the header of the first
+ *        fragment is 'C', 'A'.
  */
-const qrcom_max_nb_frags = 62;
 const qrcom_frag_idx = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const qrcom_max_nb_frags = qrcom_frag_idx.length;
